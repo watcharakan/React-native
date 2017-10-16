24bc954a0c4a3326962b2289fea75ef61d05d93e
watcharakan <job.2538@gmail.com>
2017-10-17 00:03:02 +0700

    button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import {Text, View, Image} from 'react-native';
 import Card from './Card';
-import CardSection from './CardSection'
+import CardSection from './CardSection';
+import Button from './Button'
 
 
 const AlbumDetail = ({album}) => {
 
-    const { title, artist, thumbnail_image} = album
+    const { title, artist, thumbnail_image, image} = album;
+    const {thumbnailStyle,
+             headerContentstyle,
+               thumbnailContainerStyle,
+               headrTextStyle,
+               imageStyle  } = styles
+
 return(
 
   <Card>
   <CardSection>
   <View>
       <Image 
-          style = {styles.thumbnailStyle}
+          style = {thumbnailStyle}
           source = {{uri : thumbnail_image}}/>
 
   </View>
  
-          <View style = {styles.headerContentstyle }>
-                        <Text>{title}</Text>
+          <View style = {headerContentstyle }>
+                        <Text style = {headrTextStyle}>{title}</Text>
                         <Text>{artist}</Text>
 
           </View>
   
+
+  </CardSection>
+  <CardSection>
+
+       <Image 
+       style = {imageStyle}
+       source = {{uri : image}}/>
+
+  </CardSection>
+
+  <CardSection>
+      
+        <Button/>
 
   </CardSection>
   </Card>
@@ -39,10 +59,27 @@ const styles = {
 
 
       },
+      headrTextStyle:{
+          fontSize: 18
+ 
+      },
+      imageStyle:{
+             height: 300,
+             flex: 1,
+             width: null 
+
+      },
       thumbnailStyle: {
               height: 50,
               width: 50
 
+
+      },
+      thumbnailContainerStyle:{
+           justifyContent:  'center',
+           alignItems:  'center',
+           marginLeft: 10,
+           marginRight: 10 
 
       }
 
---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Button = () =>{
+  return (
+         <Text>Click </Text>
+
+
+  	);
+
+};
+
+
+
+
+
+
+
+export default Button;
