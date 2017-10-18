e583d49bc8b560153cb968ed6fd1aa2fba37d53f
watcharakan <job.2538@gmail.com>
2017-10-18 23:56:59 +0700

    Button
    click Linking

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button'
@@ -7,7 +7,7 @@ import Button from './Button'
 
 const AlbumDetail = ({album}) => {
 
-    const { title, artist, thumbnail_image, image} = album;
+    const { title, artist, thumbnail_image, image, url} = album;
     const {thumbnailStyle,
              headerContentstyle,
                thumbnailContainerStyle,
@@ -43,8 +43,9 @@ return(
 
   <CardSection>
       
-        <Button/>
-
+        <Button onPress={() => Linking.openURL(url) }>
+        Buy Now
+        </Button>
   </CardSection>
   </Card>
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 
-const Button = () =>{
+const Button = ({onPress, children}) =>{
 	const { buttonStyle, textStyles } = styles;
   return (
-  	<TouchableOpacity onPress={() => console.log('pressed')} style = {buttonStyle }>
+  	<TouchableOpacity onPress={onPress} style = {buttonStyle }>
          <Text style ={textStyles}>
-         Click me 
+         {children} 
          </Text>
     </TouchableOpacity>
 
