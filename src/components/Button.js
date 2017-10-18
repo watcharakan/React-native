7d1ab092ff6260d729c2a05a1b162d2ff52cc019
watcharakan <job.2538@gmail.com>
2017-10-18 23:29:33 +0700

    button
    reacet buuton

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableOpacity} from 'react-native';
 
 const Button = () =>{
+	const { buttonStyle, textStyles } = styles;
   return (
-         <Text>Click </Text>
-
+  	<TouchableOpacity onPress={() => console.log('pressed')} style = {buttonStyle }>
+         <Text style ={textStyles}>
+         Click me 
+         </Text>
+    </TouchableOpacity>
 
   	);
 
@@ -12,7 +16,29 @@ const Button = () =>{
 
 
 
+const styles = {
+	textStyles:{
+		alignSelf: 'center' ,
+		color: '#007aff',
+		fontSize: 16,
+		fontWeight:   '600' ,
+		paddingTop: 10,
+		paddingBottom: 10 
 
+
+	},
+   buttonStyle:{
+   	flex: 1,
+   	alignSelf:  'stretch',
+   	backgroundColor: '#fff',
+   	borderColor: '#007aff',
+   	borderRadius: 5,
+   	borderWidth: 1,
+   	marginLeft: 5,
+   	marginRight: 5
+ 
+  } 
+};
 
 
 
