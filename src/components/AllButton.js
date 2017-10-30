0ef6f7b82ec2ecc0316cf637410bc93da5fdb759
watcharakan <job.2538@gmail.com>
2017-10-30 19:14:26 +0700

    icon

--- src/components/AllButton.js
+++ src/components/AllButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Button, Grid, Col, Row } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button, Grid, Col, Row, Icon } from 'react-native-elements';
+
 
 const AllButton = () => {
 return (
@@ -9,21 +9,32 @@ return (
   <Grid>
    <Col>
    <Row>
-   <Icon.Button name="facebook" backgroundColor="#3b5998" onPress={this.loginWithFacebook}>
-      Login with Facebook
-   </Icon.Button>
-   </Row>
-   <Row>
-     <Icon name="home" size={30} color="#4F8EF7" />
+   <Button
+     raised
+     icon={{ name: 'power-plug', type: 'entypo', size: 50 }}
+     buttonStyle={{ backgroundColor: 'blue', borderRadius: 100 }}
+     textStyle={{ textAlign: 'center' }}
+     title={'SMART \nMETER'}
+   />
    </Row>
    <Row>
    <Button
      raised
-     icon={{ name: 'home', size: 32}}
-     buttonStyle={{backgroundColor: 'red', borderRadius: 10}}
-     textStyle={{textAlign: 'center'}}
-     title={`Welcome to\nReact Native Elements`}
-   /></Row>
+     icon={{ name: 'md-speedometer', type: 'ionicon', size: 50 }}
+     buttonStyle={{ backgroundColor: 'red', borderRadius: 30 }}
+     textStyle={{ textAlign: 'center' }}
+     title={'SMART \nMETER'}
+   />
+   </Row>
+   <Row>
+   <Button
+     raised
+     icon={{ name: 'sc-telegram', type: 'evilicon', size: 50 }}
+     buttonStyle={{ backgroundColor: 'red', borderRadius: 30 }}
+     textStyle={{ textAlign: 'center' }}
+     title={'SMART \nMETER'}
+   />
+   </Row>
 
    </Col>
    <Col>
@@ -33,12 +44,6 @@ return (
 
    </Col>
 
-   <Col>
-     <Row></Row>
-     <Row></Row>
-     <Row></Row>
-
-   </Col>
   </Grid>
 
 
